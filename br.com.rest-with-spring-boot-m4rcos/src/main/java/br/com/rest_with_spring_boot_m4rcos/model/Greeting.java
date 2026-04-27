de258e117172911e4d130f0906fd820f7d14f131
Marcos Paulo <marcospaulo3107@gmail.com>
2026-04-26 21:50:22 -0300

    primeiros passos com spring boot e java 3.4.0

---
+++ br.com.rest-with-spring-boot-m4rcos/src/main/java/br/com/rest_with_spring_boot_m4rcos/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.rest_with_spring_boot_m4rcos.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ br.com.rest-with-spring-boot-m4rcos/src/test/java/br/com/rest_with_spring_boot_m4rcos/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.rest_with_spring_boot_m4rcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
